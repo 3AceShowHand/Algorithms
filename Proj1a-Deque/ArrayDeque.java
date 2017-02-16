26e53c310c582175daaa9815f58dccbb6bc15322
ChristopherKing <jinl1037@hotmail.com>
2017-02-17 00:04:14 +0800

    add a resize aux method to refactor expand and shrink.

--- Proj1a-Deque/ArrayDeque.java
+++ Proj1a-Deque/ArrayDeque.java
@@ -31,62 +31,42 @@ public class ArrayDeque<Item> implements Deque<Item> {
         return items;
     }
 
-
-    public void expand() {
-        final int EXPAND_FACTOR = 2;
-        int newCapacity = capacity * EXPAND_FACTOR ;
+    private void resize(double factor) {
+        int newCapacity = (int)(capacity * factor);
         Item[] container = (Item[]) new Object[newCapacity];
         int idx = 0;
 
-        if (front < rear) {
-            for(int i = front; i < rear; i++) {
+        if(front < rear) {
+            for (int i = front; i < rear; i++) {
                 container[idx] = items[i];
                 idx++;
             }
         } else {
-            for(int i = front; i < capacity; i++) {
+            for (int i = front; i < capacity; i++) {
                 container[idx] = items[i];
                 idx++;
             }
             for(int i = 0; i < rear; i++) {
                 container[idx] = items[i];
-                idx += 1;
+                idx++;
             }
         }
-
-        capacity = newCapacity;
         front = 0;
         rear = idx;
+        capacity = newCapacity;
         items = container;
+     }
+
+    public void expand() {
+        final int EXPAND_FACTOR = 2;
+        resize(EXPAND_FACTOR);
     }
 
     public void shrink() {
         double loadFactor = (double)size / capacity;
         if (loadFactor < 0.25 && capacity > 8) {
             final double SHRINK_FACTOR = 0.5;
-            int newCapacity = (int)(SHRINK_FACTOR * capacity);
-            Item[] container = (Item[]) new Object[newCapacity];
-            int idx = 0;
-
-            if(front > rear) {
-                for (int i = front; i < capacity; i++) {
-                    container[idx] = items[i];
-                    idx++;
-                }
-                for(int i = 0; i < rear; i++) {
-                    container[idx] = items[i];
-                    idx++;
-                }
-            } else {
-                for(int i = front; i < rear; i++) {
-                    container[idx] = items[i];
-                    idx++;
-                }
-            }
-            front = 0;
-            rear = idx;
-            capacity = newCapacity;
-            items = container;
+            resize(SHRINK_FACTOR);
         }
     }
 
