a4db5fd18e0eac3a146824c9fe0400c5da295af5
ChristopherKing <jinl1037@hotmail.com>
2017-02-24 15:19:10 +0800

    add Percolation project and scaffold.

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Christopher on 2017/2/24.
+ */
+
+/** Estimate the value of the percolation threshold via Monte Carlo simulation.*/
+public class Percolation {
+
+    /** Create n-by-n grid, and with all sites blocked */
+    public Percolation(int n) {
+
+    }
+
+    /** Open site(row, col) if it is not open already */
+    public void open(int row, int col) {
+
+    }
+
+    /** Is site(row, col) open */
+    public boolean isOpen(int row, int col) {
+
+    }
+
+    /** Is site(row, col) full */
+    public boolean isFull(int row, int col) {
+
+    }
+
+    /** Number of open sites */
+    public int numberOfOpenSite() {
+
+    }
+
+    /** Dose the system percolate*/
+    public boolean percolates() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
