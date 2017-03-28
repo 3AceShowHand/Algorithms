779a2cde30d0dab8ac146ea85b0930233769fdc9
ChristopherKing <jinl1037@hotmail.com>
2017-03-28 22:04:57 +0800

    add ArrayDeque to proj.

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,5 +1,29 @@
 /**
  * Created by Christopher on 2017/3/28.
  */
-public class Deque {
+public interface Deque<Item> {
+
+    /** Adds an item to the front of the Deque.*/
+    void addFirst(Item item);
+
+    /** Adds an item to the back of the Deque. */
+    void addLast(Item item);
+
+    /** Returns true if deque is empty, false otherwise. */
+    boolean isEmpty();
+
+    /** Returns the number of items in the Deque. */
+    int size();
+
+    /** Prints the items in the Deque from first to last, separated by a space. */
+    void printDeque();
+
+    /** Removes and returns the item at the front of the Deque. If no such item exists, returns null. */
+    Item removeFirst();
+
+    /** Removes and returns the item at the back of the Deque. If no such item exists, returns null. */
+    Item removeLast();
+
+    /** Gets the item at the given index*/
+    Item get(int index);
 }
