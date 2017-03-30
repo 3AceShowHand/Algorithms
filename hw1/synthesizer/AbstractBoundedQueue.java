3dafe494e6627d961352cf70c5c9b0b69c6921c3
ChristopherKing <jinl1037@hotmail.com>
2017-03-30 16:51:38 +0800

    add hw1 scaffold files.

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,7 @@
+package synthesizer;
+
+/**
+ * Created by Christopher on 2017/3/30.
+ */
+public abstract class AbstractBoundedQueue {
+}
---
+++ notes/Packages/src/DogLauncher.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Christopher on 2017/3/29.
+ */
+public class DogLauncher {
+}
