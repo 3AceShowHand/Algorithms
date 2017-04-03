562ff6b3678c70627e03f930e1f5944ddd132f1a
ChristopherKing <jinl1037@hotmail.com>
2017-04-04 05:36:12 +0800

    fix some typo and add exceptions to some methods.

--- hw2-Percolation/hw2/Percolation.java
+++ hw2-Percolation/hw2/Percolation.java
@@ -2,6 +2,8 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.ArrayList;
+
 public class Percolation {
 
     private site[][] grid;
@@ -36,30 +38,42 @@ public class Percolation {
         }
     }
 
+    //convert 2d index to 1d
     private int xyTo1D(int row, int col) {
         return row * grid.length + col;
     }
 
+    //get row index from 2d index
     private int getRow(int idx) {
         return idx / grid.length;
     }
 
+    //get col index from 2d index
     private int getCol(int idx) {
         return idx % grid.length;
     }
 
+    //return all valid neighbors of a certain site
+    private site[] getNeighbors(int row, int col) {
+
+    }
+
     public void open(int row, int col) {
-        if (row < 0 || row >= grid.length) || (col < 0 || col >= grid.length)
+        if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length))
                 throw new IndexOutOfBoundsException("row or col idx must between 0 and " + (grid.length - 1));
         grid[row][col].open = true;
         numberOfOpenSite += 1;
     }
 
     public boolean isOpen(int row, int col) {
+        if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length))
+            throw new IndexOutOfBoundsException("row or col idx must between 0 and " + (grid.length - 1));
         return grid[row][col].open;
     }
 
     public boolean isFull(int row, int col) {
+        if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length))
+            throw new IndexOutOfBoundsException("row or col idx must between 0 and " + (grid.length - 1));
         return grid[row][col].full;
     }
 
