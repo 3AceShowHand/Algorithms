aed9ba17b329d83ae49dbd7ba07b96c719e4a188
ChristopherKing <jinl1037@hotmail.com>
2017-04-24 16:09:05 +0800

    add comments to FastCollinearPoints class.

--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -6,14 +6,17 @@ public class FastCollinearPoints {
 
     int count;
 
+    /** find all line segments containing 4 points */
     public FastCollinearPoints(Point[] points) {
         this.count = 0;
     }
 
+    /** the number of line segments */
     public int numberOfSegments() {
         return count;
     }
 
+    /** the line segments */
     public LineSegment[] segments() {
         return null;
     }
