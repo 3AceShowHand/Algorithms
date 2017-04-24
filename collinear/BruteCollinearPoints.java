3c37d60eed6e2266789ecc7af38e3d2c9addd3e2
ChristopherKing <jinl1037@hotmail.com>
2017-04-24 16:08:11 +0800

    add basic scaffold code.

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -3,4 +3,24 @@
  * Written:    2017/4/23
  */
 public class BruteCollinearPoints {
+
+    int count;
+    Point[] points;
+
+    /** find all line segments containing 4 points */
+    public BruteCollinearPoints(Point[] points) {
+        this.count = 0;
+        this.points = points;
+    }
+
+    /** the number of line segments */
+    public int numberOfSegments() {
+        return count;
+    }
+
+    /** the line segments */
+    public LineSegment[] segments() {
+        return null;
+    }
+
 }
--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -3,4 +3,19 @@
  * Written:    2017/4/23
  */
 public class FastCollinearPoints {
+
+    int count;
+
+    public FastCollinearPoints(Point[] points) {
+        this.count = 0;
+    }
+
+    public int numberOfSegments() {
+        return count;
+    }
+
+    public LineSegment[] segments() {
+        return null;
+    }
+
 }
