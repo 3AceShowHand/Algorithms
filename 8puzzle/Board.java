5ade6f5fed739f069244526565521e95f226ce2d
ChristopherKing <jinl1037@hotmail.com>
2017-05-14 00:49:12 +0800

    add Board and Solver scaffolds to the problem.

---
+++ 8puzzle/Board.java
@@ -0,0 +1,43 @@
+/**
+ * Author:     Christopher
+ * Written:    2017/5/14
+ */
+public class Board {
+
+    public Board(int[][] blocks) {
+
+    }
+
+    public int dimension() {
+        return 0;
+    }
+
+    public int hamming() {
+        return 0;
+    }
+
+    public int manhattan() {
+        return 0;
+    }
+
+    public boolean isGoal() {
+        return false;
+    }
+
+    public Board twin() {
+        return null;
+    }
+
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    public Iterable<Board> neighbors() {
+        return null;
+    }
+
+    public String toString() {
+        return null;
+    }
+
+}
---
+++ 8puzzle/Solver.java
@@ -0,0 +1,22 @@
+/**
+ * Author:     Christopher
+ * Written:    2017/5/14
+ */
+public class Solver {
+
+    public Solver(Board initial) {
+
+    }
+
+    public boolean isSolvable() {
+        return false;
+    }
+
+    public int moves() {
+        return 0;
+    }
+
+    public Iterable<Board> solution() {
+        return null;
+    }
+}
