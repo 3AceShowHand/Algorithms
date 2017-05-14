3bc9f6119996a33501ab731f2c37191261617aca
ChristopherKing <jinl1037@hotmail.com>
2017-05-14 13:46:28 +0800

    add comments to 8puzzle.

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -4,6 +4,7 @@
  */
 public class Board {
 
+    // construct a board from an n-by-n array of blocks
     public Board(int[][] blocks) {
 
     }
@@ -12,26 +13,32 @@ public class Board {
         return 0;
     }
 
+    // number of blocks out of place
     public int hamming() {
         return 0;
     }
 
+    // sum of Manhattan distances between blocks and goal
     public int manhattan() {
         return 0;
     }
 
+    // is this board the goal board?
     public boolean isGoal() {
         return false;
     }
 
+    // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
         return null;
     }
+
 
     public boolean equals(Object y) {
         return false;
     }
 
+    // all neighboring boards
     public Iterable<Board> neighbors() {
         return null;
     }
