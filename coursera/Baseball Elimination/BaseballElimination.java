df3c7e00e0a4c67b87e7786f6fd01909ccec8b4a
JinLingChristopher <jinl1037@hotmail.com>
2017-12-27 16:05:29 +0800

    add basic methods scaffold to baseball project.

--- coursera/Baseball Elimination/BaseballElimination.java
+++ coursera/Baseball Elimination/BaseballElimination.java
@@ -2,4 +2,42 @@
 
 public class BaseballElimination {
 
+    private int num;
+
+    public BaseballElimination(String filename) {
+
+    }
+
+    public int numberOfTeams() {
+        return num;
+    }
+
+    public Iterable<String> teams() {
+        return null;
+    }
+
+    public int wins(String team) {
+        return 0;
+    }
+
+    public int losses(String team) {
+        return 0;
+    }
+
+    public int remaining(String team) {
+        return 0;
+    }
+
+    public int against(String team1, String team2) {
+        return 0;
+    }
+
+    public boolean isEliminated(String team) {
+        return false;
+    }
+
+    public Iterable<String> certificateOfElimination(String team) {
+        return null;
+    }
+
 }
