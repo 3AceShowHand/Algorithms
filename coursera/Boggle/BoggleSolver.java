d9164cf972a49d2dcb665dc62de70715832e8523
JinLingChristopher <jinl1037@hotmail.com>
2018-03-13 11:31:06 +0800

    add scaffold for boggle.

---
+++ coursera/Boggle/BoggleSolver.java
@@ -0,0 +1,51 @@
+/**
+ * @author king
+ */
+
+import edu.princeton.cs.algs4.SET;
+
+public class BoggleSolver {
+
+    private final SET<String> dict;
+
+    public BoggleSolver(String[] dictionary) {
+        if (dictionary == null) {
+            throw new IllegalArgumentException("Given dictionary is invalid.");
+        }
+        dict = new SET();
+        for (String item: dictionary) {
+            dict.add(item);
+        }
+    }
+
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        if (board == null) {
+            throw new IllegalArgumentException("Given board is invalid");
+        }
+        return null;
+    }
+
+    // return the score of the given word if it is in the dictionary, zero otherwise.
+    public int scoreOf(String word) {
+        if (word == null) {
+            throw new IllegalArgumentException("Given word is invalid");
+        }
+        int length = word.length();
+        if (!dict.contains(word)) {
+            return 0;
+        } else if (length <= 2) {
+            return 0;
+        } else if (length <= 4) {
+            return 1;
+        } else if (length == 5) {
+            return 2;
+        } else if (length == 6) {
+            return 3;
+        } else if (length == 7) {
+            return 5;
+        } else {
+            return 11;
+        }
+    }
+
+}
