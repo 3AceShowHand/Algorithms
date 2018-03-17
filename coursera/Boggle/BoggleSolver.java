ce4f1380bea4eb738324739f69ddd50bfe0e7017
JinLingChristopher <jinl1037@hotmail.com>
2018-03-17 17:41:03 +0800

    Add some code to BoggleSolver.

--- coursera/Boggle/BoggleSolver.java
+++ coursera/Boggle/BoggleSolver.java
@@ -3,20 +3,21 @@
  */
 
 import edu.princeton.cs.algs4.SET;
-
-import java.util.ArrayList;
+import edu.princeton.cs.algs4.TST;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class BoggleSolver {
 
-    private final SET<String> dict;
+    private final TST<Integer> dict;
 
     public BoggleSolver(String[] dictionary) {
+        fisherYatesShuffle(dictionary);
         if (dictionary == null) {
             throw new IllegalArgumentException("Given dictionary is invalid.");
         }
-        dict = new SET();
+        dict = new TST<>();
         for (String item: dictionary) {
-            dict.add(item);
+            dict.put(item, scoreOf(item));
         }
     }
 
@@ -24,7 +25,14 @@ public class BoggleSolver {
         if (board == null) {
             throw new IllegalArgumentException("Given board is invalid");
         }
-        ArrayList<String> validWords = new ArrayList<>();
+
+        boolean marked[][] = new boolean[board.rows()][board.cols()];
+        SET<String> validWords = new SET<>();
+
+        StringBuilder prefix = new StringBuilder();
+
+        dfs(board, 0, 0, prefix, marked, validWords);
+
         return validWords;
     }
 
@@ -34,9 +42,7 @@ public class BoggleSolver {
             throw new IllegalArgumentException("Given word is invalid");
         }
         int length = word.length();
-        if (!dict.contains(word)) {
-            return 0;
-        } else if (length <= 2) {
+        if (!dict.contains(word) || length <= 2) {
             return 0;
         } else if (length <= 4) {
             return 1;
@@ -49,6 +55,78 @@ public class BoggleSolver {
         } else {
             return 11;
         }
+    }
+
+    private void dfs(BoggleBoard board, int row, int col, StringBuilder prefix, boolean[][] marked, SET<String> result) {
+        if (marked[row][col]) {
+            return;
+        }
+
+        char c = board.getLetter(row, col);
+        if (c == 'Q') {
+            prefix.append("QU");
+        } else {
+            prefix.append(c);
+        }
+        String word = prefix.toString();
+
+        if (dict.keysWithPrefix(word).) {
+            if (c == 'Q') {
+                prefix.deleteCharAt(prefix.length()-1);
+                prefix.deleteCharAt(prefix.length()-1);
+            } else {
+                prefix.deleteCharAt(prefix.length()-1);
+            }
+            return;
+        }
+
+        if (word.length() > 2 && dict.contains(word)) {
+            result.add(word);
+        }
+        marked[row][col] = true;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else {
+                    int newrow = row + i;
+                    int newcol = col + j;
+                    if (newrow >= 0 && newrow < board.rows() && newcol >= 0 && newcol < board.cols() && !marked[newrow][newcol]) {
+                        dfs(board, newrow, newcol, prefix, marked, result);
+                    }
+                }
+            }
+        }
+        marked[row][col] = false;
+    }
+
+    private void fisherYatesShuffle(String[] strs) {
+        // implementation of fisher-Yates shuffle algorithm
+        int last = strs.length-1;
+        for (int i = 0; i < last; last--) {
+            int idx = StdRandom.uniform(last+1);
+            String t = strs[last];
+            strs[last] = strs[idx];
+            strs[idx] = t;
+        }
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+
+        BoggleBoard board = new BoggleBoard(args[1]);
+
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        int score = 0;
+
+        Iterable<String> validWords = solver.getAllValidWords(board);
+        for (String word : validWords) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
     }
 
 }
