13ac84f338c069a2f945f0cb7fd5a97bc00904b7
JinLingChristopher <jinl1037@hotmail.com>
2018-03-20 00:16:52 +0800

    add java files.

---
+++ coursera/Burrows-Wheeler/CircularSuffixArray.java
@@ -0,0 +1,40 @@
+public class CircularSuffixArray {
+
+<<<<<<< HEAD
+    // circular suffix array of s
+=======
+>>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
+    public CircularSuffixArray(String s) {
+
+    }
+
+<<<<<<< HEAD
+    // length of s
+    public int length() {
+        return -1;
+    }
+
+    // returns index of ith sorted suffix
+=======
+    public int length() {
+        return 0;
+    }
+
+>>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
+    public int index(int i) {
+        return -1;
+    }
+
+<<<<<<< HEAD
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+}
+=======
+    public static void main(String[] args) {
+
+    }
+
+}
+>>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
