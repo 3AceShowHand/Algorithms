b794eb2c538d6e1721084a9d35880f4c2ff7df22
JinLingChristopher <jinl1037@hotmail.com>
2018-03-19 15:21:56 +0800

    Add scaffold for burrowswheeler .

---
+++ coursera/Burrows-Wheeler/BurrowsWheeler.java
@@ -0,0 +1,15 @@
+public class BurrowsWheeler {
+
+    public static void transform() {
+
+    }
+
+    public static void inverseTransform() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ coursera/Burrows-Wheeler/CircularSuffixArray.java
@@ -0,0 +1,19 @@
+public class CircularSuffixArray {
+
+    public CircularSuffixArray(String s) {
+
+    }
+
+    public int length() {
+        return 0;
+    }
+
+    public int index(int i) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
