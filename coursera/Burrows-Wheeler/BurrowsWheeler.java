fa94869abd6706ed50ca6b5ebbd47bf29a4157c3
JinLingChristopher <jinl1037@hotmail.com>
2018-03-28 00:19:21 +0800

    complete basic CircularSuffixArray class.

--- coursera/Burrows-Wheeler/BurrowsWheeler.java
+++ coursera/Burrows-Wheeler/BurrowsWheeler.java
@@ -1,32 +1,18 @@
 public class BurrowsWheeler {
 
-<<<<<<< HEAD
     // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
-=======
->>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
     public static void transform() {
 
     }
 
-<<<<<<< HEAD
     // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
-=======
->>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
     public static void inverseTransform() {
 
     }
 
-<<<<<<< HEAD
     // if args[0] is '-', apply Burrows-Wheeler transform
     // if args[0] is '+', apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
 
     }
 }
-=======
-    public static void main(String[] args) {
-
-    }
-
-}
->>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
--- coursera/Burrows-Wheeler/CircularSuffixArray.java
+++ coursera/Burrows-Wheeler/CircularSuffixArray.java
@@ -1,40 +1,70 @@
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class CircularSuffixArray {
 
-<<<<<<< HEAD
-    // circular suffix array of s
-=======
->>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
+    private String value;
+    private String[] suffixes;
+    private Integer[] index;
+
     public CircularSuffixArray(String s) {
+        value = new String(s);
+        suffixes = new String[value.length()];
+        for (int i = 0; i < value.length(); i++) {
+            String suffix = value.substring(i, value.length()) + value.substring(0, i);
+            suffixes[i] = suffix;
+        }
+        ArrayIndexComparator comparator = new ArrayIndexComparator(suffixes);
+        index = comparator.createIndexArray();
+        Arrays.sort(index, comparator);
+    }
+
+    private class ArrayIndexComparator implements Comparator<Integer> {
+        private final String[] array;
+
+        public ArrayIndexComparator(String[] arr) {
+            array = arr;
+        }
+
+        public Integer[] createIndexArray() {
+            Integer[] index = new Integer[array.length];
+            for (int i = 0; i < index.length; i++) {
+                index[i] = i;
+            }
+            return index;
+        }
+
+        @Override
+        public int compare(Integer a, Integer b) {
+            return array[a].compareTo(array[b]);
+        }
 
     }
 
-<<<<<<< HEAD
     // length of s
     public int length() {
-        return -1;
+        return value.length();
     }
 
-    // returns index of ith sorted suffix
-=======
-    public int length() {
-        return 0;
-    }
 
->>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
     public int index(int i) {
-        return -1;
+        return index[i];
     }
 
-<<<<<<< HEAD
     // unit testing (required)
     public static void main(String[] args) {
+        String s = "ABRACADABRA!";
+        CircularSuffixArray t = new CircularSuffixArray(s);
+        for (int i = 0; i < t.suffixes.length; i++) {
+            StdOut.println(t.suffixes[i]);
+        }
+
+        for (int i = 0; i < t.index.length; i++) {
+            StdOut.println(t.index[i]);
+        }
 
     }
-}
-=======
-    public static void main(String[] args) {
-
-    }
 
 }
->>>>>>> b794eb2c538d6e1721084a9d35880f4c2ff7df22
