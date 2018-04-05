023b4236500a29da92f0ed6f913f55747c1ab5ce
JinLingChristopher <jinl1037@hotmail.com>
2018-04-06 01:36:26 +0800

    rewrite CircularSuffixArray.

--- coursera/Burrows-Wheeler/CircularSuffixArray.java
+++ coursera/Burrows-Wheeler/CircularSuffixArray.java
@@ -1,70 +1,89 @@
 import edu.princeton.cs.algs4.StdOut;
-
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 public class CircularSuffixArray {
 
-    private String value;
-    private String[] suffixes;
-    private Integer[] index;
+    private ArrayList<Integer> indexes;
 
     public CircularSuffixArray(String s) {
-        value = new String(s);
-        suffixes = new String[value.length()];
-        for (int i = 0; i < value.length(); i++) {
-            String suffix = value.substring(i, value.length()) + value.substring(0, i);
-            suffixes[i] = suffix;
+        if (s == null) {
+            throw new IllegalArgumentException("invalid null input constructor.");
         }
-        ArrayIndexComparator comparator = new ArrayIndexComparator(suffixes);
-        index = comparator.createIndexArray();
-        Arrays.sort(index, comparator);
+
+        // given input s, maintain a map of characters c in s to indices of c
+        TreeMap<Character, ArrayList<Integer>> map = new TreeMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) {
+                map.put(c, new ArrayList<>());
+            }
+            map.get(c).add(i);
+        }
+
+        indexes = new ArrayList<>();
+
+        ArrayIndexComparator comparator = new ArrayIndexComparator(s);
+        for (Map.Entry<Character, ArrayList<Integer>> pairs: map.entrySet()) {
+            ArrayList<Integer> values = pairs.getValue();
+            values.sort(comparator);
+            indexes.addAll(values);
+        }
     }
 
     private class ArrayIndexComparator implements Comparator<Integer> {
-        private final String[] array;
 
-        public ArrayIndexComparator(String[] arr) {
-            array = arr;
+        private final String s;
+
+        ArrayIndexComparator(String s) {
+            this.s = s;
         }
 
-        public Integer[] createIndexArray() {
-            Integer[] index = new Integer[array.length];
-            for (int i = 0; i < index.length; i++) {
-                index[i] = i;
-            }
-            return index;
+        private int getNextIndex(int i) {
+            return (i + 1) % s.length();
         }
 
         @Override
         public int compare(Integer a, Integer b) {
-            return array[a].compareTo(array[b]);
-        }
+            int aNext = getNextIndex(a);
+            int bNext = getNextIndex(b);
 
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(aNext) == s.charAt(bNext)) {
+                    aNext = getNextIndex(aNext);
+                    bNext = getNextIndex(bNext);
+                } else if (s.charAt(aNext) < s.charAt(bNext)) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+            return 0;
+        }
     }
 
     // length of s
     public int length() {
-        return value.length();
+        return indexes.size();
     }
 
 
     public int index(int i) {
-        return index[i];
+        if (i > length()) {
+            throw new IllegalArgumentException("i is out of index's range.");
+        }
+        return indexes.get(i);
     }
 
     // unit testing (required)
     public static void main(String[] args) {
         String s = "ABRACADABRA!";
         CircularSuffixArray t = new CircularSuffixArray(s);
-        for (int i = 0; i < t.suffixes.length; i++) {
-            StdOut.println(t.suffixes[i]);
-        }
 
-        for (int i = 0; i < t.index.length; i++) {
-            StdOut.println(t.index[i]);
-        }
-
+        StdOut.printf("the length of s is: %s\n", t.length());
+        StdOut.println(t.indexes);
     }
-
 }
--- coursera/Burrows-Wheeler/MoveToFront.java
+++ coursera/Burrows-Wheeler/MoveToFront.java
@@ -1,7 +1,10 @@
+
+
+
 public class MoveToFront {
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-
+        // maintain an ordered sequence of 256 extended ASCII characters.
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
