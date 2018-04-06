3d980247445ea0578f5783c9696c3544a2d343f9
JinLingChristopher <jinl1037@hotmail.com>
2018-04-06 23:32:32 +0800

    complete basic BurrowsWheeler transform method, maybe bugs need to be fixed.

--- coursera/Burrows-Wheeler/BurrowsWheeler.java
+++ coursera/Burrows-Wheeler/BurrowsWheeler.java
@@ -3,31 +3,27 @@ import edu.princeton.cs.algs4.BinaryStdIn;
 public class BurrowsWheeler {
 
     // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
-//    public static void transform() {
-//        char input = BinaryStdIn.readChar();
-//        CircularSuffixArray csa = new CircularSuffixArray(input);
-//
-//        char endup = input.charAt(input.length() - 1);
-//        StringBuilder sb = new StringBuilder();
-//        int first = -1;
-//        for (int i = 0; i < csa.length(); i++) {
-//            int index = csa.index(i);
-//            char c = input.charAt(index);
-//            if (first == -1 && c == endup) {
-//                first = index;
-//            }
-//            sb.append(c);
-//        }
-//
-//        String transformed = sb.toString();
-//
-//        BinaryStdOut.write(first);
-//        BinaryStdOut.write(transformed);
-//        BinaryStdOut.close();
-//    }
+    public static void transform() {
+        String input = BinaryStdIn.readString();
+        CircularSuffixArray csa = new CircularSuffixArray(input);
 
-    public static void tranform() {
+        char endup = input.charAt(input.length() - 1);
+        StringBuilder sb = new StringBuilder();
+        int first = -1;
+        for (int i = 0; i < csa.length(); i++) {
+            int index = csa.index(i);
+            char c = input.charAt(index);
+            if (first == -1 && c == endup) {
+                first = index;
+            }
+            sb.append(c);
+        }
 
+        String transformed = sb.toString();
+
+        BinaryStdOut.write(first);
+        BinaryStdOut.write(transformed);
+        BinaryStdOut.close();
     }
 
     // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
