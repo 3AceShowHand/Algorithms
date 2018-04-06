67947090c52c0d53a590499a6c473829260a546d
JinLingChristopher <jinl1037@hotmail.com>
2018-04-07 01:52:34 +0800

    complete basic BurrowsWheeler inverseTransorm method, maybe bugs need to be fixed or code optimization.

--- coursera/Burrows-Wheeler/BurrowsWheeler.java
+++ coursera/Burrows-Wheeler/BurrowsWheeler.java
@@ -1,4 +1,8 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BurrowsWheeler {
 
@@ -27,14 +31,39 @@ public class BurrowsWheeler {
     }
 
     // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
+    // the inputs to inverseTransform is output of transform.
     public static void inverseTransform() {
-        String input = BinaryStdIn.readString();
+        String input = BinaryStdIn.readString();                    // t
+        int first = BinaryStdIn.readInt();
 
+        char[] firstColumn = (new String(input)).toCharArray();
+        Arrays.sort(firstColumn);
+        ArrayList<Character> sortedFirst = new ArrayList<>();
+        for (char c: firstColumn) {
+            sortedFirst.add(c);
+        }
+
+        int[] next = new int[input.length()];
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(first);
+            int index = sortedFirst.indexOf(c);
+            next[index] = first;
+            first = index;
+        }
+        for (int i = 0; i < next.length; i++) {
+            BinaryStdOut.write(next[i]);
+        }
+        BinaryStdOut.close();
     }
 
     // if args[0] is '-', apply Burrows-Wheeler transform
     // if args[0] is '+', apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
-
+        if (args[0] == "-") {
+            transform();
+        } else {
+            inverseTransform();
+        }
     }
 }
